f41d2864e443a76feee93175927af3100d09f73e
ahmadhridoy <badaruddinahmadridoy11@gmail.com>
2022-05-22 12:11:42 +0600

    admin added by hridoy

---
+++ admin/index.php
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <title>my-kitchen website -Home Page</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
---
+++ admin/partials/footer.php
@@ -0,0 +1,9 @@
+<div class="footer">
+            <div class="wrapper">
+                <p class="text-center">2020 All rights reserved . Developed By - <a href="#">Badar Uddin Ahmad Ridoy</a></p>
+            </div>
+        </div>
+    
+
+    </body>
+</html>
