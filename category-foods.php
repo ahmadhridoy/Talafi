69bba5efce70c76b9f268ecce9b37a39b5a4213e
ahmadhridoy <badaruddinahmadridoy11@gmail.com>
2022-06-12 22:04:24 +0600

    update category-food files

--- category-foods.php
+++ category-foods.php
@@ -27,7 +27,7 @@
     ?>
 
 
-    <!-- fOOD sEARCH Section Starts Here -->
+    
     <section class="food-search text-center">
         <div class="container">
             
@@ -35,30 +35,29 @@
 
         </div>
     </section>
-    <!-- fOOD sEARCH Section Ends Here -->
+    
 
 
 
-    <!-- fOOD MEnu Section Starts Here -->
+    
     <section class="food-menu">
         <div class="container">
             <h2 class="text-center">Food Menu</h2>
 
             <?php 
             
-                //Create SQL Query to Get foods based on Selected CAtegory
+                
                 $sql2 = "SELECT * FROM tbl_food WHERE category_id=$category_id";
 
-                //Execute the Query
+                
                 $res2 = mysqli_query($conn, $sql2);
 
-                //Count the Rows
                 $count2 = mysqli_num_rows($res2);
 
-                //CHeck whether food is available or not
+                
                 if($count2>0)
                 {
-                    //Food is Available
+                    
                     while($row2=mysqli_fetch_assoc($res2))
                     {
                         $id = $row2['id'];
@@ -73,12 +72,12 @@
                                 <?php 
                                     if($image_name=="")
                                     {
-                                        //Image not Available
+                                        
                                         echo "<div class='error'>Image not Available.</div>";
                                     }
                                     else
                                     {
-                                        //Image Available
+                                        
                                         ?>
                                         <img src="<?php echo SITEURL; ?>images/food/<?php echo $image_name; ?>" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
                                         <?php
@@ -104,7 +103,7 @@
                 }
                 else
                 {
-                    //Food not available
+        
                     echo "<div class='error'>Food not Available.</div>";
                 }
             
@@ -119,6 +118,6 @@
         </div>
 
     </section>
-    <!-- fOOD Menu Section Ends Here -->
+    
 
     <?php include('partials-front/footer.php'); ?>
